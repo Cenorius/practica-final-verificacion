8b5d8174021b13a03fa5ae7f79b40d1922123276
M. Ángel <miguelangel4b@gmail.com>
2017-05-28 20:00:38 +0200

    Update frontend.
    Now it asks for a date and makes the user choose between processing also remote news or just the already downloaded ones

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, DateField, RadioField
 
 class TextProcessorForm(Form):
-    name = TextAreaField('Name: ', validators=[validators.required()])
+    date = DateField('Date: ', validators=[validators.required()])
+    source = RadioField('Source', choices=[('Local','Process only already downloaded news'),('Remote','Get remote news and process them')])

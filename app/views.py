73836acb93144ac7bf320d92d156cc1c5f5af7a7
Cenorius <dave.ayuso@gmail.com>
2017-06-03 19:54:29 +0200

    add backend

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, request
 from app import app
 from .forms import TextProcessorForm
+import pymongo
+
 from textprocessor import core
 from DBUtils import DBUtils
 from scrapper import scrapper
@@ -12,14 +14,14 @@ DATA_BASE_NAME='words'
 @app.route("/", methods=['GET', 'POST'])
 def parse_text():
     temp=[]
-    articles = None
-    words=None
+    articles_r =[]
+    words_r=[]
 
     form = TextProcessorForm(request.form)
 
     if request.method == 'POST':
 
-        db=DBUtils(MongoClient(URL_MONGO)[DATA_BASE_NAME])
+        db=DBUtils(pymongo.MongoClient(URL_MONGO)[DATA_BASE_NAME].collection)
 
         date = form.date.raw_data[0]
         temp=date.split('/')
@@ -33,7 +35,16 @@ def parse_text():
                         words=scrapper.get_article_body(article.get('url'))
                         words=core.process(words)
                         db.insert_words(date,article.get('title'),words)
-                
+                words_r=db.get_words_from_date(date)
+
             elif request.form['source']=='Articles':
-                print "palabras por articulo"
-    return render_template('index.html', form=form, words=words, articles=articles)
+                articles=scrapper.get_articles_by_date(date)
+                for article in articles:
+                    if not db.exists_article_in_db(article.get('title'),date):
+                        words=scrapper.get_article_body(article.get('url'))
+                        words=core.process(words)
+                        db.insert_words(date,article.get('title'),words)
+                    articles_r.append({u"title":article.get("title"), u'words':db.get_words_from_article(date,article.get('title'))})
+                    
+                print articles_r[0]
+    return render_template('index.html', form=form, words=words_r, articles=articles_r)
